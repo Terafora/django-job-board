d85600b4266bd6d3624bdda22597cd481d7662ba
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2023-11-08 08:22:07 +0000

    re-setup dependencies for project on desktop

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from job_board.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
